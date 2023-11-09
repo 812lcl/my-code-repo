d8bd0b1000d5ba6123e51cab391c5c402f70a4ea
812lcl <812liuchunlei@163.com>
2023-11-09 15:49:52 +0800

    fix 3sum UT

--- leetcode/0015.3Sum/3Sum_test.go
+++ leetcode/0015.3Sum/3Sum_test.go
@@ -47,3 +47,42 @@ func Test_threeSum(t *testing.T) {
 		})
 	}
 }
+func Test_threeSum3(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			name: "case 1",
+			args: args{
+				nums: []int{-1, 0, 1, 2, -1, -4},
+			},
+			want: [][]int{{-1, 0, 1}, {-1, -1, 2}},
+		},
+		{
+			name: "case 2",
+			args: args{
+				nums: []int{0, 0, 0},
+			},
+			want: [][]int{{0, 0, 0}},
+		},
+		{
+			name: "case 3",
+			args: args{
+				nums: []int{1, 2, 3, 4, 5},
+			},
+			want: [][]int{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := threeSum3(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("threeSum3() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
