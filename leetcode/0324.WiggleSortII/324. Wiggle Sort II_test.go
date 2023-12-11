6dd3b242557b55fb82cfa81965eaba4cc45267b8
812lcl <812liuchunlei@163.com>
2023-12-11 14:20:12 +0800

    fix UT

--- leetcode/0324.WiggleSortII/324. Wiggle Sort II_test.go
+++ leetcode/0324.WiggleSortII/324. Wiggle Sort II_test.go
@@ -14,12 +14,12 @@ func Test_wiggleSort(t *testing.T) {
 		{
 			name: "case 1",
 			nums: []int{1, 5, 1, 1, 6, 4},
-			want: []int{1, 4, 1, 5, 1, 6},
+			want: []int{1, 6, 1, 5, 1, 4},
 		},
 		{
 			name: "case 2",
 			nums: []int{1, 3, 2, 2, 3, 1},
-			want: []int{1, 2, 1, 3, 2, 3},
+			want: []int{2, 3, 1, 3, 1, 2},
 		},
 		{
 			name: "case 3",
